4ae63c71078ecf5ceadc0668a6e5000369ec1aa7
VDK <VDK@DESKTOP-QSTC9T8.fritz.box>
2023-01-19 12:35:52 -0600

    Creacion del proyecto inicial

---
+++ src/main/java/com/TiendaM_1Q2023/TiendaM1Q2023Application.java
@@ -0,0 +1,13 @@
+package com.TiendaM_1Q2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaM1Q2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaM1Q2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/TiendaM_1Q2023/TiendaM1Q2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaM_1Q2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaM1Q2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
